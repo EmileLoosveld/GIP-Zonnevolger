dc0e14aa6986a4492796b5c0d04f493c14a91020
Emile Loosveld <emile.loosveld@sgsintpaulus.eu>
2021-11-29 17:55:46 +0100

    Add project files.

---
+++ GIP-Zonnevolger/frmHoofd.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace GIP_Zonnevolger
+{
+    partial class frmHoofd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHoofd));
+            this.pnlOpties = new System.Windows.Forms.Panel();
+            this.grpWerking = new System.Windows.Forms.GroupBox();
+            this.rdbAutomatisch = new System.Windows.Forms.RadioButton();
+            this.rdbManueel = new System.Windows.Forms.RadioButton();
+            this.pnlBesturing = new System.Windows.Forms.Panel();
+            this.pnlAfbeelding = new System.Windows.Forms.Panel();
+            this.btnNoord = new System.Windows.Forms.Button();
+            this.btnWest = new System.Windows.Forms.Button();
+            this.btnOost = new System.Windows.Forms.Button();
+            this.btnZuid = new System.Windows.Forms.Button();
+            this.grpBesturing = new System.Windows.Forms.GroupBox();
+            this.grpTijdBeweging = new System.Windows.Forms.GroupBox();
+            this.txtTijd = new System.Windows.Forms.TextBox();
+            this.lblTijd = new System.Windows.Forms.Label();
+            this.lblTijd2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grpInformatie = new System.Windows.Forms.GroupBox();
+            this.grpSpanningen = new System.Windows.Forms.GroupBox();
+            this.lblNoord = new System.Windows.Forms.Label();
+            this.lblZuid = new System.Windows.Forms.Label();
+            this.lblOost = new System.Windows.Forms.Label();
+            this.lblWest = new System.Windows.Forms.Label();
+            this.lblNoordWaarde = new System.Windows.Forms.Label();
+            this.lblZuidWaarde = new System.Windows.Forms.Label();
+            this.lblOostWaarde = new System.Windows.Forms.Label();
+            this.lblWestWaarde = new System.Windows.Forms.Label();
+            this.lblV = new System.Windows.Forms.Label();
+            this.lblV2 = new System.Windows.Forms.Label();
+            this.lblV3 = new System.Windows.Forms.Label();
+            this.lblV4 = new System.Windows.Forms.Label();
+            this.grpWindsnelheid = new System.Windows.Forms.GroupBox();
+            this.pnlOpties.SuspendLayout();
+            this.grpWerking.SuspendLayout();
+            this.pnlBesturing.SuspendLayout();
+            this.pnlAfbeelding.SuspendLayout();
+            this.grpBesturing.SuspendLayout();
+            this.grpTijdBeweging.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpInformatie.SuspendLayout();
+            this.grpSpanningen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlOpties
+            // 
+            this.pnlOpties.Controls.Add(this.grpInformatie);
+            this.pnlOpties.Controls.Add(this.grpTijdBeweging);
+            this.pnlOpties.Controls.Add(this.grpWerking);
+            resources.ApplyResources(this.pnlOpties, "pnlOpties");
+            this.pnlOpties.Name = "pnlOpties";
+            // 
+            // grpWerking
+            // 
+            resources.ApplyResources(this.grpWerking, "grpWerking");
+            this.grpWerking.Controls.Add(this.rdbAutomatisch);
+            this.grpWerking.Controls.Add(this.rdbManueel);
+            this.grpWerking.Name = "grpWerking";
+            this.grpWerking.TabStop = false;
+            // 
+            // rdbAutomatisch
+            // 
+            resources.ApplyResources(this.rdbAutomatisch, "rdbAutomatisch");
+            this.rdbAutomatisch.Name = "rdbAutomatisch";
+            this.rdbAutomatisch.TabStop = true;
+            this.rdbAutomatisch.UseVisualStyleBackColor = true;
+            // 
+            // rdbManueel
+            // 
+            resources.ApplyResources(this.rdbManueel, "rdbManueel");
+            this.rdbManueel.Name = "rdbManueel";
+            this.rdbManueel.TabStop = true;
+            this.rdbManueel.UseVisualStyleBackColor = true;
+            // 
+            // pnlBesturing
+            // 
+            this.pnlBesturing.Controls.Add(this.grpBesturing);
+            resources.ApplyResources(this.pnlBesturing, "pnlBesturing");
+            this.pnlBesturing.Name = "pnlBesturing";
+            // 
+            // pnlAfbeelding
+            // 
+            this.pnlAfbeelding.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.pnlAfbeelding, "pnlAfbeelding");
+            this.pnlAfbeelding.Name = "pnlAfbeelding";
+            // 
+            // btnNoord
+            // 
+            resources.ApplyResources(this.btnNoord, "btnNoord");
+            this.btnNoord.Name = "btnNoord";
+            this.btnNoord.UseVisualStyleBackColor = true;
+            // 
+            // btnWest
+            // 
+            resources.ApplyResources(this.btnWest, "btnWest");
+            this.btnWest.Name = "btnWest";
+            this.btnWest.UseVisualStyleBackColor = true;
+            // 
+            // btnOost
+            // 
+            resources.ApplyResources(this.btnOost, "btnOost");
+            this.btnOost.Name = "btnOost";
+            this.btnOost.UseVisualStyleBackColor = true;
+            // 
+            // btnZuid
+            // 
+            resources.ApplyResources(this.btnZuid, "btnZuid");
+            this.btnZuid.Name = "btnZuid";
+            this.btnZuid.UseVisualStyleBackColor = true;
+            // 
+            // grpBesturing
+            // 
+            resources.ApplyResources(this.grpBesturing, "grpBesturing");
+            this.grpBesturing.Controls.Add(this.btnZuid);
+            this.grpBesturing.Controls.Add(this.btnNoord);
+            this.grpBesturing.Controls.Add(this.btnWest);
+            this.grpBesturing.Controls.Add(this.btnOost);
+            this.grpBesturing.Name = "grpBesturing";
+            this.grpBesturing.TabStop = false;
+            // 
+            // grpTijdBeweging
+            // 
+            resources.ApplyResources(this.grpTijdBeweging, "grpTijdBeweging");
+            this.grpTijdBeweging.Controls.Add(this.lblTijd2);
+            this.grpTijdBeweging.Controls.Add(this.lblTijd);
+            this.grpTijdBeweging.Controls.Add(this.txtTijd);
+            this.grpTijdBeweging.Name = "grpTijdBeweging";
+            this.grpTijdBeweging.TabStop = false;
+            // 
+            // txtTijd
+            // 
+            resources.ApplyResources(this.txtTijd, "txtTijd");
+            this.txtTijd.Name = "txtTijd";
+            // 
+            // lblTijd
+            // 
+            resources.ApplyResources(this.lblTijd, "lblTijd");
+            this.lblTijd.Name = "lblTijd";
+            // 
+            // lblTijd2
+            // 
+            resources.ApplyResources(this.lblTijd2, "lblTijd2");
+            this.lblTijd2.Name = "lblTijd2";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // grpInformatie
+            // 
+            resources.ApplyResources(this.grpInformatie, "grpInformatie");
+            this.grpInformatie.Controls.Add(this.grpWindsnelheid);
+            this.grpInformatie.Controls.Add(this.grpSpanningen);
+            this.grpInformatie.Name = "grpInformatie";
+            this.grpInformatie.TabStop = false;
+            // 
+            // grpSpanningen
+            // 
+            this.grpSpanningen.Controls.Add(this.lblV4);
+            this.grpSpanningen.Controls.Add(this.lblV3);
+            this.grpSpanningen.Controls.Add(this.lblV2);
+            this.grpSpanningen.Controls.Add(this.lblV);
+            this.grpSpanningen.Controls.Add(this.lblWestWaarde);
+            this.grpSpanningen.Controls.Add(this.lblOostWaarde);
+            this.grpSpanningen.Controls.Add(this.lblZuidWaarde);
+            this.grpSpanningen.Controls.Add(this.lblNoordWaarde);
+            this.grpSpanningen.Controls.Add(this.lblWest);
+            this.grpSpanningen.Controls.Add(this.lblOost);
+            this.grpSpanningen.Controls.Add(this.lblZuid);
+            this.grpSpanningen.Controls.Add(this.lblNoord);
+            resources.ApplyResources(this.grpSpanningen, "grpSpanningen");
+            this.grpSpanningen.Name = "grpSpanningen";
+            this.grpSpanningen.TabStop = false;
+            // 
+            // lblNoord
+            // 
+            resources.ApplyResources(this.lblNoord, "lblNoord");
+            this.lblNoord.Name = "lblNoord";
+            // 
+            // lblZuid
+            // 
+            resources.ApplyResources(this.lblZuid, "lblZuid");
+            this.lblZuid.Name = "lblZuid";
+            // 
+            // lblOost
+            // 
+            resources.ApplyResources(this.lblOost, "lblOost");
+            this.lblOost.Name = "lblOost";
+            // 
+            // lblWest
+            // 
+            resources.ApplyResources(this.lblWest, "lblWest");
+            this.lblWest.Name = "lblWest";
+            // 
+            // lblNoordWaarde
+            // 
+            resources.ApplyResources(this.lblNoordWaarde, "lblNoordWaarde");
+            this.lblNoordWaarde.Name = "lblNoordWaarde";
+            // 
+            // lblZuidWaarde
+            // 
+            resources.ApplyResources(this.lblZuidWaarde, "lblZuidWaarde");
+            this.lblZuidWaarde.Name = "lblZuidWaarde";
+            // 
+            // lblOostWaarde
+            // 
+            resources.ApplyResources(this.lblOostWaarde, "lblOostWaarde");
+            this.lblOostWaarde.Name = "lblOostWaarde";
+            // 
+            // lblWestWaarde
+            // 
+            resources.ApplyResources(this.lblWestWaarde, "lblWestWaarde");
+            this.lblWestWaarde.Name = "lblWestWaarde";
+            // 
+            // lblV
+            // 
+            resources.ApplyResources(this.lblV, "lblV");
+            this.lblV.Name = "lblV";
+            // 
+            // lblV2
+            // 
+            resources.ApplyResources(this.lblV2, "lblV2");
+            this.lblV2.Name = "lblV2";
+            // 
+            // lblV3
+            // 
+            resources.ApplyResources(this.lblV3, "lblV3");
+            this.lblV3.Name = "lblV3";
+            // 
+            // lblV4
+            // 
+            resources.ApplyResources(this.lblV4, "lblV4");
+            this.lblV4.Name = "lblV4";
+            // 
+            // grpWindsnelheid
+            // 
+            resources.ApplyResources(this.grpWindsnelheid, "grpWindsnelheid");
+            this.grpWindsnelheid.Name = "grpWindsnelheid";
+            this.grpWindsnelheid.TabStop = false;
+            // 
+            // frmHoofd
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlAfbeelding);
+            this.Controls.Add(this.pnlBesturing);
+            this.Controls.Add(this.pnlOpties);
+            this.Name = "frmHoofd";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlOpties.ResumeLayout(false);
+            this.grpWerking.ResumeLayout(false);
+            this.grpWerking.PerformLayout();
+            this.pnlBesturing.ResumeLayout(false);
+            this.pnlAfbeelding.ResumeLayout(false);
+            this.grpBesturing.ResumeLayout(false);
+            this.grpTijdBeweging.ResumeLayout(false);
+            this.grpTijdBeweging.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grpInformatie.ResumeLayout(false);
+            this.grpSpanningen.ResumeLayout(false);
+            this.grpSpanningen.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlOpties;
+        private System.Windows.Forms.GroupBox grpWerking;
+        private System.Windows.Forms.Panel pnlBesturing;
+        private System.Windows.Forms.Panel pnlAfbeelding;
+        private System.Windows.Forms.RadioButton rdbAutomatisch;
+        private System.Windows.Forms.RadioButton rdbManueel;
+        private System.Windows.Forms.GroupBox grpBesturing;
+        private System.Windows.Forms.Button btnZuid;
+        private System.Windows.Forms.Button btnNoord;
+        private System.Windows.Forms.Button btnWest;
+        private System.Windows.Forms.Button btnOost;
+        private System.Windows.Forms.GroupBox grpInformatie;
+        private System.Windows.Forms.GroupBox grpSpanningen;
+        private System.Windows.Forms.GroupBox grpTijdBeweging;
+        private System.Windows.Forms.Label lblTijd2;
+        private System.Windows.Forms.Label lblTijd;
+        private System.Windows.Forms.TextBox txtTijd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblV4;
+        private System.Windows.Forms.Label lblV3;
+        private System.Windows.Forms.Label lblV2;
+        private System.Windows.Forms.Label lblV;
+        private System.Windows.Forms.Label lblWestWaarde;
+        private System.Windows.Forms.Label lblOostWaarde;
+        private System.Windows.Forms.Label lblZuidWaarde;
+        private System.Windows.Forms.Label lblNoordWaarde;
+        private System.Windows.Forms.Label lblWest;
+        private System.Windows.Forms.Label lblOost;
+        private System.Windows.Forms.Label lblZuid;
+        private System.Windows.Forms.Label lblNoord;
+        private System.Windows.Forms.GroupBox grpWindsnelheid;
+    }
+}
+
